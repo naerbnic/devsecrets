4a248efea506d82f4e1e786d9b37f3ad6257ba42
Brian Chin <brian.chin@gmail.com>
2020-02-26 16:09:23 -0800

    Refactor packages, and move functions to where it's necessary

--- cargo-devsecrets/src/main.rs
+++ cargo-devsecrets/src/main.rs
@@ -1,7 +1,33 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 mod cli;
 mod workspace;
+
+pub fn init_devsecrets_dir_from_manifest_dir(
+    manifest_dir: impl AsRef<Path>,
+) -> anyhow::Result<PathBuf> {
+    let id = devsecrets_core::ensure_devsecrets_id(manifest_dir)?;
+    let root = devsecrets_core::DevSecretsRootDir::ensure_new()?;
+    let child = root.ensure_child(&id)?;
+    Ok(child.path().to_path_buf())
+}
+
+pub fn get_devsecrets_dir_from_manifest_dir(
+    manifest_dir: impl AsRef<Path>,
+) -> anyhow::Result<Option<PathBuf>> {
+    let id = devsecrets_core::read_devsecrets_id(manifest_dir)?
+        .ok_or(anyhow::anyhow!("Could not read devsecrets id from project"))?;
+    let root = match devsecrets_core::DevSecretsRootDir::new()? {
+        Some(root) => root,
+        None => return Ok(None),
+    };
+
+    let child = match root.get_child(&id)? {
+        Some(child) => child,
+        None => return Ok(None),
+    };
+    Ok(Some(child.path().to_path_buf()))
+}
 
 fn main() {
     env_logger::init();
@@ -24,12 +50,12 @@ fn main() {
     let manifest_dir = &curr_package.manifest_path.parent().unwrap();
 
     if let Some(_) = matches.subcommand_matches("init") {
-        match devsecrets::init_devsecrets_dir_from_manifest_dir(manifest_dir) {
+        match init_devsecrets_dir_from_manifest_dir(manifest_dir) {
             Ok(dir) => println!("Dir: {}", dir.to_str().unwrap()),
             Err(e) => println!("Unable to init directory: {}", e),
         }
     } else if let Some(_) = matches.subcommand_matches("path") {
-        match devsecrets::get_devsecrets_dir_from_manifest_dir(manifest_dir) {
+        match get_devsecrets_dir_from_manifest_dir(manifest_dir) {
             Ok(Some(dir)) => println!("{}", dir.to_str().unwrap()),
             Ok(None) => println!("Devsecrets dir has not be initialized. Run init."),
             Err(e) => println!("Unable to find devsecrets directory: {:#}", e),
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,52 @@
 use serde::de::DeserializeOwned;
 use std::path::{Component, Path, PathBuf};
 
+// Re-export the devsecrets_id macro to make it available to users.
 pub use devsecrets_macros::devsecrets_id;
-
+// Re-export the DevSecretsId struct to be used as an Id
 pub use devsecrets_core::DevSecretsId as Id;
+
+#[derive(thiserror::Error, Debug)]
+pub enum SecretCreateError {
+    #[error("Could not find child dir: {0}")]
+    ChildDirNotFound(#[source] std::io::Error),
+
+    #[error("Could not find root dir: {0}")]
+    RootDirNotFound(#[source] std::io::Error),
+}
+
+/// A general error for DevSecrets operations.
+#[derive(thiserror::Error, Debug)]
+pub enum SecretReadError {
+    #[error("Got invalid relative path: {0}")]
+    InvalidRelativePath(String),
+
+    #[error("Invalid extension: {0}")]
+    InvalidExtension(String),
+
+    #[error("File error: {0}")]
+    FileError(#[from] std::io::Error),
+
+    #[error("Could not parse file data: {0}")]
+    ParseError(#[source] Box<dyn std::error::Error + Send + Sync + 'static>),
+}
 
 pub struct DevSecrets {
     dir: devsecrets_core::DevSecretsDir,
 }
 
 impl DevSecrets {
-    pub fn from_id(id: &Id) -> anyhow::Result<Option<Self>> {
-        let root = match devsecrets_core::DevSecretsRootDir::new()? {
+    pub fn from_id(id: &Id) -> std::result::Result<Option<Self>, SecretCreateError> {
+        let root = match devsecrets_core::DevSecretsRootDir::new()
+            .map_err(SecretCreateError::ChildDirNotFound)?
+        {
             Some(root) => root,
             None => return Ok(None),
         };
-        let child = match root.get_child(id)? {
+        let child = match root
+            .get_child(id)
+            .map_err(SecretCreateError::RootDirNotFound)?
+        {
             Some(child) => child,
             None => return Ok(None),
         };
@@ -27,17 +58,25 @@ impl DevSecrets {
         self.dir.path()
     }
 
-    fn get_relative_path(&self, relpath: impl AsRef<Path>) -> anyhow::Result<PathBuf> {
+    fn get_relative_path(&self, relpath: impl AsRef<Path>) -> Result<PathBuf, SecretReadError> {
         let relpath = relpath.as_ref();
         if relpath.is_absolute() {
-            anyhow::bail!("Path {:?} must not be absolute.", relpath);
+            return Err(SecretReadError::InvalidRelativePath(format!(
+                "Path {:?} must not be absolute.",
+                relpath
+            )));
         }
 
         // Check that we only have normal parts of the path
         for component in relpath.components() {
             match component {
                 Component::Normal(_) => (),
-                _ => anyhow::bail!("Path {:?} has a non-normal component.", relpath),
+                _ => {
+                    return Err(SecretReadError::InvalidRelativePath(format!(
+                        "Path {:?} has a non-normal component.",
+                        relpath
+                    )))
+                }
             }
         }
 
@@ -47,40 +86,18 @@ impl DevSecrets {
     pub fn read_json_secret<T: DeserializeOwned, P: AsRef<Path>>(
         &self,
         path: P,
-    ) -> anyhow::Result<T> {
+    ) -> Result<T, SecretReadError> {
         let path = path.as_ref();
         if path.extension() != Some(std::ffi::OsStr::new("json")) {
-            anyhow::bail!("Path {:?} must have a .json extension.", path)
+            return Err(SecretReadError::InvalidExtension(format!(
+                "Path {:?} must have a .json extension.",
+                path
+            )));
         }
         let fullpath = self.get_relative_path(path)?;
         log::info!("Reading json secret from {:?}", fullpath);
-        let contents = std::fs::read_to_string(fullpath)?;
-        Ok(serde_json::from_str::<T>(&contents)?)
+        let contents = std::fs::read_to_string(fullpath).map_err(SecretReadError::FileError)?;
+        Ok(serde_json::from_str::<T>(&contents)
+            .map_err(|e| SecretReadError::ParseError(Box::new(e)))?)
     }
-}
-
-pub fn init_devsecrets_dir_from_manifest_dir(
-    manifest_dir: impl AsRef<Path>,
-) -> anyhow::Result<PathBuf> {
-    let id = devsecrets_core::ensure_devsecrets_id(manifest_dir)?;
-    let root = devsecrets_core::DevSecretsRootDir::ensure_new()?;
-    let child = root.ensure_child(&id)?;
-    Ok(child.path().to_path_buf())
-}
-
-pub fn get_devsecrets_dir_from_manifest_dir(
-    manifest_dir: impl AsRef<Path>,
-) -> anyhow::Result<Option<PathBuf>> {
-    let id = devsecrets_core::read_devsecrets_id(manifest_dir)?
-        .ok_or(anyhow::anyhow!("Could not read devsecrets id from project"))?;
-    let root = match devsecrets_core::DevSecretsRootDir::new()? {
-        Some(root) => root,
-        None => return Ok(None),
-    };
-
-    let child = match root.get_child(&id)? {
-        Some(child) => child,
-        None => return Ok(None),
-    };
-    Ok(Some(child.path().to_path_buf()))
 }
